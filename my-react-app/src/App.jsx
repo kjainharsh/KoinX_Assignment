ec510590ab2882f0a5be8d13d38cecd51ae22178
kjainharsh <kjainharsh@gmail.com>
2025-01-10 16:39:14 +0530

    Navbar Added

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Navbar from "./components/Navbar";
+import CoinPage from "./pages/CoinPage";
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <Navbar />
+      <CoinPage />
     </div>
   );
 }
---
+++ my-react-app/src/pages/CoinPage.jsx
@@ -0,0 +1,9 @@
+function CoinPage() {
+    return (
+        <div className="h-screen w-screen bg-slate-200/40">
+            {/* Content goes here */}
+        </div>
+    );
+}
+
+export default CoinPage;
